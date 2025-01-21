2554e73bf1d607db7a3fc90892d3961a20629ada
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-01-21 04:42:00 -0800

    feat: update client limiter poc

--- client-limiter-poc/src/test/kotlin/limiter/FixedWindowLimiterTest.kt
+++ client-limiter-poc/src/test/kotlin/limiter/FixedWindowLimiterTest.kt
@@ -37,4 +37,32 @@ class FixedWindowLimiterTest {
         assertTrue(result3)
         assertFalse(result4)
     }
+
+    @Test
+    fun shouldAllowRequestAfterSomeTime() {
+        // Arrange
+        val fixedWindowLimiter = FixedWindowLimiter()
+        val requestName = "test"
+
+        // Act
+        val result1 = fixedWindowLimiter.request(requestName)
+        val result2 = fixedWindowLimiter.request(requestName)
+        val result3 = fixedWindowLimiter.request(requestName)
+        val result4 = fixedWindowLimiter.request(requestName)
+        Thread.sleep(1000)
+        val result5 = fixedWindowLimiter.request(requestName)
+        val result6 = fixedWindowLimiter.request(requestName)
+        val result7 = fixedWindowLimiter.request(requestName)
+        val result8 = fixedWindowLimiter.request(requestName)
+
+        // Assert
+        assertTrue(result1)
+        assertTrue(result2)
+        assertTrue(result3)
+        assertFalse(result4)
+        assertTrue(result5)
+        assertTrue(result6)
+        assertTrue(result7)
+        assertFalse(result8)
+    }
 }
