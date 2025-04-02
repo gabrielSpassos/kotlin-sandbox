0fb2759f068485c8f2265fc832225278e48b31c1
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-04-02 04:32:13 -0700

    update math kotlin poc

--- math-poc/src/main/kotlin/MyMath.kt
+++ math-poc/src/main/kotlin/MyMath.kt
@@ -1,9 +1,30 @@
 package com.gabrielspassos
 
+import kotlin.math.ceil
+
 object MyMath {
 
     fun divideIntRound(value: Long, divisor: Long): Long {
         val result = value.toDouble() / divisor.toDouble()
-        return Math.ceil(result).toLong()
+        return ceil(result).toLong()
+    }
+
+    fun splitGroups(group: Int): Pair<Long, Long> {
+        val totalPages = 50
+        val pagesToProcess = divideIntRound(totalPages.toLong(), 5)
+
+        val firstPage = if (1 == group) {
+            1
+        } else {
+            val temp = group - 1
+            (pagesToProcess * temp) + 1
+        }
+        val lastPage = pagesToProcess * group
+
+        println("Total pages: $totalPages")
+        println("PagesToProcess: $pagesToProcess")
+        println("First page: $firstPage LastPage: $lastPage")
+
+        return Pair(firstPage, lastPage)
     }
 }
--- math-poc/src/test/kotlin/MyMathTest.kt
+++ math-poc/src/test/kotlin/MyMathTest.kt
@@ -28,4 +28,52 @@ class MyMathTest {
     fun testDivide4() {
         assertEquals(4, MyMath.divideIntRound(17, 5))
     }
+
+    @Test
+    fun testSplitGroups() {
+        val pair = MyMath.splitGroups(1)
+
+        assertEquals(1, pair.first)
+        assertEquals(10, pair.second)
+    }
+
+    @Test
+    fun testSplitGroups2() {
+        val pair = MyMath.splitGroups(2)
+
+        assertEquals(11, pair.first)
+        assertEquals(20, pair.second)
+    }
+
+    @Test
+    fun testSplitGroups3() {
+        val pair = MyMath.splitGroups(3)
+
+        assertEquals(21, pair.first)
+        assertEquals(30, pair.second)
+    }
+
+    @Test
+    fun testSplitGroups4() {
+        val pair = MyMath.splitGroups(4)
+
+        assertEquals(31, pair.first)
+        assertEquals(40, pair.second)
+    }
+
+    @Test
+    fun testSplitGroups5() {
+        val pair = MyMath.splitGroups(5)
+
+        assertEquals(41, pair.first)
+        assertEquals(50, pair.second)
+    }
+
+    @Test
+    fun testSplitGroups6() {
+        val pair = MyMath.splitGroups(6)
+
+        assertEquals(51, pair.first)
+        assertEquals(60, pair.second)
+    }
  }
