a9ca47754b0166f348264168a831c0070f4657e1
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-04-08 16:44:10 -0700

    test: add tests

--- text-justification/src/test/kotlin/TextJustificationTest.kt
+++ text-justification/src/test/kotlin/TextJustificationTest.kt
@@ -1,2 +1,59 @@
+import com.gabrielspassos.TextJustification
 import org.junit.jupiter.api.Assertions.*
- class TextJustificationTest
+import org.junit.jupiter.api.Test
+
+class TextJustificationTest {
+
+    @Test
+    fun example1() {
+        val textJustification = TextJustification()
+        val words = listOf("This", "is", "an", "example", "of", "text")
+        val maxWidth = 16
+        val expected = listOf(
+            "This    is    an",
+            "example  of text",
+            "justification.  "
+        )
+
+        val result = textJustification.justify(words, maxWidth)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun example2() {
+        val textJustification = TextJustification()
+        val words = listOf("What", "must", "be", "acknowledgment", "shall", "be")
+        val maxWidth = 16
+        val expected = listOf(
+            "What   must   be",
+            "acknowledgment  ",
+            "shall be        "
+        )
+
+        val result = textJustification.justify(words, maxWidth)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun example3() {
+        val textJustification = TextJustification()
+
+        val words = listOf("Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a",
+            "computer.", "Art", "is", "everything", "else", "we", "do")
+        val maxWidth = 20
+        val expected = listOf(
+            "Science  is  what we",
+            "understand      well",
+            "enough to explain to",
+            "a  computer.  Art is",
+            "everything  else  we",
+            "do                  "
+        )
+
+        val result = textJustification.justify(words, maxWidth)
+
+        assertEquals(expected, result)
+    }
+}
