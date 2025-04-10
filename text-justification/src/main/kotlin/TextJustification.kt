f98a25af2fdf0aada3ee52ad2cc76dce34290935
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-04-09 18:50:40 -0700

    feat: update test justification poc

--- text-justification/src/main/kotlin/TextJustification.kt
+++ text-justification/src/main/kotlin/TextJustification.kt
@@ -3,7 +3,28 @@ package com.gabrielspassos
 class TextJustification {
 
     fun justify(words: List<String>, maxWidth: Int): List<String> {
-        return listOf()
+        val lines = mutableListOf<String>()
+        var line = ""
+        var lineBuffer = 0
+
+        for (word in words) {
+            if (word.length + lineBuffer + 1 <= maxWidth) {
+                val wordWithSpace = word.plus(" ")
+                line += wordWithSpace
+                lineBuffer += wordWithSpace.length
+            } else {
+                lines.add(line.trimEnd())
+                line = ""
+                val wordWithSpace = word.plus(" ")
+                line += wordWithSpace
+                lineBuffer = 0
+                lineBuffer += wordWithSpace.length
+            }
+        }
+
+        lines.add(line.trimEnd())
+
+        return lines
     }
 
 }
