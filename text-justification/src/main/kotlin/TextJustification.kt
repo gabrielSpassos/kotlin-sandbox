29d29863e4c7604b1e560c3690cd6bcd31b6a42e
Gabriel Passos <gabrielsantos45725@gmail.com>
2025-04-29 22:41:16 -0300

    refact: refactor text justification poc

--- text-justification/src/main/kotlin/TextJustification.kt
+++ text-justification/src/main/kotlin/TextJustification.kt
@@ -4,27 +4,37 @@ class TextJustification {
 
     fun justify(words: List<String>, maxWidth: Int): List<String> {
         val lines = mutableListOf<String>()
-        var line = ""
+        val line = mutableListOf<String>()
         var lineBuffer = 0
 
         for (word in words) {
-            if (lineBuffer + word.length + 1 <= maxWidth) {
-                val wordWithSpace = word.plus(" ")
-                line += wordWithSpace
-                lineBuffer += wordWithSpace.length
-            } else {
-                lines.add(line.trimEnd())
-                line = ""
+            if (lineBuffer + word.length + 1 > maxWidth) {
+                val wordsInLine = line.size
+                val missingSpaces = (maxWidth - lineBuffer) % wordsInLine
+                val spaces = createSpaces(missingSpaces)
+                println("Missing spaces $missingSpaces: $spaces")
+                lines.add(line.joinToString(spaces).trimEnd())
+                line.clear()
                 lineBuffer = 0
-                val wordWithSpace = word.plus(" ")
-                line += wordWithSpace
-                lineBuffer += wordWithSpace.length
+                line.add(word)
+                lineBuffer += (word.length + 1)
+            } else {
+                line.add(word)
+                lineBuffer += (word.length + 1)
             }
         }
 
-        lines.add(line.trimEnd())
+        lines.add(line.joinToString(" ").trimEnd())
 
         return lines
+    }
+
+    private fun createSpaces(times: Int): String {
+        var spaces = ""
+        for (i in 1..times) {
+            spaces += "*"
+        }
+        return spaces
     }
 
 }
