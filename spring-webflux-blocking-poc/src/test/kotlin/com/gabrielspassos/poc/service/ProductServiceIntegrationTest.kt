4ba2662bb520106d7754fd7676d7c21ab9971703
gabrielSpassos <gabrielsantos45725@gmail.com>
2025-12-03 23:27:27 -0300

    refact: refactor imports

--- spring-webflux-blocking-poc/src/test/kotlin/com/gabrielspassos/poc/service/ProductServiceIntegrationTest.kt
+++ spring-webflux-blocking-poc/src/test/kotlin/com/gabrielspassos/poc/service/ProductServiceIntegrationTest.kt
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.assertNotNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
