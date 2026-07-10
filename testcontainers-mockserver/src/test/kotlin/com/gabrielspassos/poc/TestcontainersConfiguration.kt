0c738c4a201efa42142f0214d6d970732d1c9caa
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-07-10 19:35:50 -0300

    feat: add test containers mock server poc

--- testcontainers-mockserver/src/test/kotlin/com/gabrielspassos/poc/TestcontainersConfiguration.kt
+++ testcontainers-mockserver/src/test/kotlin/com/gabrielspassos/poc/TestcontainersConfiguration.kt
@@ -1,6 +1,19 @@
 package com.gabrielspassos.poc
 
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.mockserver.MockServerContainer
+import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection(name = "mockserver")
+    fun mockServerContainer(): MockServerContainer {
+        val container = MockServerContainer(DockerImageName.parse("mockserver/mockserver:7.4.0"))
+        container.start()
+        return container
+    }
+}
