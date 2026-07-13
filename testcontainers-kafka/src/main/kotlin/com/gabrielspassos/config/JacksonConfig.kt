af1ebf005e2d35f8ff03149c95207d5ee666a8fa
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-07-13 10:20:16 -0300

    feat: add test containers kafka poc

--- testcontainers-kafka/src/main/kotlin/com/gabrielspassos/config/JacksonConfig.kt
+++ testcontainers-kafka/src/main/kotlin/com/gabrielspassos/config/JacksonConfig.kt
@@ -1,11 +1,18 @@
 package com.gabrielspassos.config
 
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import tools.jackson.databind.ObjectMapper
+import tools.jackson.module.kotlin.KotlinModule
 
 @Configuration
-class JacksonConfig(objectMapper: ObjectMapper) {
-    init {
-        objectMapper.registerKotlinModule()
+class JacksonConfig() {
+
+    @Bean
+    fun jacksonCustomizer(): JsonMapperBuilderCustomizer {
+        return JsonMapperBuilderCustomizer { builder ->
+            builder.addModules(KotlinModule.Builder().build())
+        }
     }
+
 }
